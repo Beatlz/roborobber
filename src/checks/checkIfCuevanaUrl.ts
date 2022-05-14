beb898e342c1edfd7b5fd5b84770e32f779da0f8
beatlz <adrian@basave.com>
2022-05-14 13:22:59 +0200

    First check created

---
+++ src/checks/checkIfCuevanaUrl.ts
@@ -0,0 +1,10 @@
+require(`module-alias/register`)
+
+const checkIfCuevanaUrl = (url: string): boolean => {
+  const regex = /^https:\/\/(www\.)?cuevana2\.io\/pelicula\/([a-zA-Z0-9_-]+)\//
+  const match = regex.test(url)
+
+  return match
+}
+
+export default checkIfCuevanaUrl
