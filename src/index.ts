b6d2f7bb5a2bfd4bcf7082fe5ea9f37c7541691a
beatlz <adrian@basave.com>
2022-05-15 12:14:57 +0200

    Full runtime completed with NO autoload

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import checkIfCuevanaUrl from "@checks/checkIfCuevanaUrl"
 const run = async (): Promise<void> => {
 	console.log(chalk.green(`Starting browser`))
 
+	const browser = await puppeteer.launch({ headless: true })
+
 	try {
 		const args = process.argv.slice(2)
 		const movie = args[0]
@@ -22,14 +24,14 @@ const run = async (): Promise<void> => {
 		const movieName = movie.split("/pelicula/").pop()
 		
 		console.log(chalk.magenta(`Scraping ${movieName}`))
-
-		const browser = await puppeteer.launch({ headless: false })
 		const url = await scrapeIframeUrl(browser, movie)
 		
 		console.log(chalk.green(`Enjoy ${movieName}!`))
 		console.log(chalk.blue(url))
 	} catch (error: any) {
 		console.log(chalk.red(error.message))
+	} finally {
+		await browser.close()
 	}
 }
 
--- src/scrapers/scrapeIframeUrl.ts
+++ src/scrapers/scrapeIframeUrl.ts
@@ -1,26 +1,52 @@
 require(`module-alias/register`)
 
 import { Browser } from "puppeteer"
+import chalk from "chalk"
+import checkIfMovieUrl from "@checks/checkIfMovieUrl"
 
 const scrapeIframeUrl = async (browser: Browser, url: string): Promise<string> => {
     try {
 			const page = await browser.newPage()
 
       await page.goto(url)
-      // await page.setRequestInterception(true)
-      
-      // page.on('request', request => {
-      //   console.log(request.url())
-      // })
-      page.waitForSelector('#cmc')
+      await page.waitForSelector('#cmc')
 
       const src = await page.evaluate((): string => {
         return document.querySelector('#cmc')?.getAttribute('src') || ``
       })
+      
+      await page.goto(`https:${src}`)
+      await page.waitForSelector('#start')
 
-      return `https:${src}`
-    } catch (error) {
-      return ``
+      page.click('#start')
+
+      await page.waitForSelector(`#PlayerDisplay`)
+
+      const movieUrlList = await page.evaluate((): string[] => {
+        const movieListElements = Object.values(document.querySelectorAll(`[onclick*="go_to_player"]`))
+        
+        return movieListElements.map(movie => movie.getAttribute(`onclick`)!)
+      })
+
+      console.log(chalk.magenta(`Found this urls:`))
+
+      movieUrlList.forEach(movieUrl => console.log(chalk.blue(movieUrl)))
+
+      const movieUrl = checkIfMovieUrl(movieUrlList[0])
+
+      console.log(movieUrl)
+
+      if (!movieUrl) {
+        throw new TypeError(`No movie url found`)
+      }
+
+      const movieUrlMatch = checkIfMovieUrl(movieUrl)
+
+      return movieUrlMatch || `No movie url found`
+    } catch (error: any) {
+      console.log(chalk.red(error.message))
+      
+      return error
     }
 }
 
