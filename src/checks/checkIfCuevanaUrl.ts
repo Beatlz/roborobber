441f7bc36b61fcdfa948488b20b052f36cd07973
beatlz <adrian@basave.com>
2022-05-15 12:43:17 +0200

    Bugfix: tvseries urls fixed

--- src/checks/checkIfCuevanaUrl.ts
+++ src/checks/checkIfCuevanaUrl.ts
@@ -1,5 +1,5 @@
 const checkIfCuevanaUrl = (url: string): boolean => {
-  const regex = /^https:\/\/(www\.)?cuevana2\.io\/pelicula\/([a-zA-Z0-9_-]+)\//
+  const regex = /^https:\/\/(www\.)?cuevana2\.io\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)\//
   const match = regex.test(url)
 
   return match
